65ffb274076debb80f520f548fe1b4403ee10876
buathi_q <buathi_q@epitech.eu>
2014-07-02 14:43:41 +0200

    Update regexp in separators text

--- config/separators.js
+++ config/separators.js
@@ -5,9 +5,9 @@ module.exports = {
   ],
 
   text: [
-    /\n?De\s*:\s*/i, // Outlook Web App
-    /\n?-{2,}\s*[a-z0-9 ]*(forward)|(original)[a-z0-9 ]*\s*-{2,}/i, // Gmail
-    /\n?From\s*:\s/i,
+    /\nDe\s*:\s*/i, // Outlook Web App
+    /\n-{2,}\s*[a-z0-9 ]*(forward)|(original)[a-z0-9 ]*\s*-{2,}/i, // Gmail
+    /\nFrom\s*:\s/i,
     /\n[_-]{4,}/,
     /\non(.+)wrote:\n/i,
   ]
