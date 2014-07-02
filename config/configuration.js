dc232b1282a9e91d775390a88c665e2f1ab9358f
buathi_q <buathi_q@epitech.eu>
2014-07-02 14:33:56 +0200

    Put separators config in another file

--- config/configuration.js
+++ config/configuration.js
@@ -21,16 +21,5 @@ module.exports = {
 
   concurrency: process.env.EMBED_MAIL_CONCURRENCY || default_concurrency,
 
-  separators_html: [
-    "<div class=\"yahoo_quoted\">",
-    "<div class=\"gmail_extra\">",
-  ],
-
-  separators_text: [
-    /\n?De\s*:\s*/i, // Outlook Web App
-    /-{2,}\s*[a-z0-9 ]*(forward)|(original)[a-z0-9 ]*\s*-{2,}/i, // Gmail
-    /\n?From\s*:\s/i,
-    /\n[_-]{4,}/,
-    /\non(.+)wrote:\n/i,
-  ]
+  separators: require('./separators.js')
 };
---
+++ config/separators.js
@@ -0,0 +1,14 @@
+module.exports = {
+  html: [
+    "<div class=\"yahoo_quoted\">",
+    "<div class=\"gmail_extra\">",
+  ],
+
+  text: [
+    /\n?De\s*:\s*/i, // Outlook Web App
+    /-{2,}\s*[a-z0-9 ]*(forward)|(original)[a-z0-9 ]*\s*-{2,}/i, // Gmail
+    /\n?From\s*:\s/i,
+    /\n[_-]{4,}/,
+    /\non(.+)wrote:\n/i,
+  ]
+};
